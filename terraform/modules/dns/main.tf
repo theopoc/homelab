2d8dff2e21c39d743280604676369a49de219393
Sofiane Djerbi <sofiane.djerbi38@gmail.com>
2025-12-07 19:43:38 +0100

    fix: point root domain to gateway IP for ingress traffic
    Previously etcd.me pointed to origin_ip (control plane) while
    subdomains pointed to gateway_ip. Now both use gateway_ip when set.

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -29,13 +29,13 @@ data "aws_route53_zone" "this" {
   private_zone = false
 }
 
-# DNS A record pointing to origin IP
+# DNS A record pointing to gateway IP (for ingress traffic) or origin IP
 resource "aws_route53_record" "this" {
   zone_id = data.aws_route53_zone.this.zone_id
   name    = var.domain
   type    = "A"
   ttl     = 300
-  records = [var.origin_ip]
+  records = [coalesce(var.gateway_ip, var.origin_ip)]
 }
 
 # Additional subdomain A records (e.g., argo.etcd.me)
